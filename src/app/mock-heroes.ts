1361f21112bca25c2add310495d2099d2b2a7164
saikrishna <vamshi.sai.9866@gmail.com>
2018-12-09 11:50:02 +0530

    basic design

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,11 @@
+    import {Hero} from './hero'
+
+    export const HEROES :Hero[] = [
+        {id:1,name:'sai'},
+        {id:2,name:'krishna'},
+        {id:3,name:'sandeep'},
+        {id:4,name:'navya'},
+        {id:5,name:'venky'},
+        {id:6,name:'sangeetha'}
+    ];
+        
